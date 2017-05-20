8a9864635fc8675a2737e3a48c4f4a626ba7a344
JosefaHerrera <josefa@linuxero.cl>
2017-05-20 14:07:42 -0400

    style

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function ValidarCampos(){
+if(nombre=="" || apellido=="" || correo=="" || direccion=="" || telefono=="" || pack==""){
+		alert("Error, todos los campos son obligatorios");
+	}else{
+	alert("Muchas gracias por suscribirte!")
+
+};
